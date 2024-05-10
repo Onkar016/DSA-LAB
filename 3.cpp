330f4c6d4724efbfbddaae559acb1d591c36a700
Onkar016 <135893444+Onkar016@users.noreply.github.com>
2024-05-10 11:22:43 +0530

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+
+    public:
+
+        string data;
+        Node* left;
+        Node* right;
+
+    Node(string d)
+    {
+            data = d;
+            left = NULL;
+            right = NULL;
+    }
+
+};
+
+void printbook(Node* root,int diagonal)
+{
+    if(!root)
+    {
+        return;
+    }
+
+    for(int i =0 ; i<diagonal; i++)
+    {
+        cout<<"\t";
+    }
+
+    cout<<root->data<<endl;
+
+    printbook(root->left,diagonal+1);
+
+    printbook(root->right,diagonal);
+
+
+}
+int main()
+{
+              string bookName;
+    int noOfChapters;
+    cout << "Enter Name of the book: ";
+    cin >> bookName;
+    cout << "Enter Number of Chapters in the book: ";
+    cin >> noOfChapters;
+
+
+            Node* root = new Node(bookName);
+
+             string chapterName;
+    int noOfSections;
+
+            Node* prev = NULL;
+
+            for(int i =0 ; i<noOfChapters; i++)
+            {
+                     cout << "Enter Name for chapter " << i+1 << ": ";
+        cin >> chapterName;
+        cout << "Enter No of sections in chapter " << i+1 << ": ";
+        cin >> noOfSections;
+
+                    Node* curr = new Node(chapterName);
+
+                    if(prev == NULL)
+                    {
+                        root->left = curr;
+                    }
+                    else
+                    {
+                        prev->right = curr;
+                    }
+
+                     string sectionName;   
+                    prev = NULL;
+
+                    for(int j=0 ; j<noOfSections;j++)
+                    {
+
+                              cout << "Enter Name for section " << j+1 << " chapter " << i+1 << ": ";
+            cin >> sectionName;
+
+                            Node* secNode = new Node(sectionName);
+
+                               if(prev == NULL)
+                    {
+                        curr->left = secNode;
+                    }
+                    else
+                    {
+                        prev->right = secNode;
+                    }
+
+                    prev = secNode;
+
+
+                    }
+
+                    prev = curr;
+
+            }
+
+            printbook(root,0);
+
+            cout<<endl;
+
+
+
+}
